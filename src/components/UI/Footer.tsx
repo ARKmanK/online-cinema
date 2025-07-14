42b8e6c7fa4a16219b6626b8e35df92bc7cc7523
ARKman <mr.korepanov.antosha@mail.ru>
2025-07-14 16:15:34 +0800

    add HomePage, PageNotFound

---
+++ src/components/UI/Footer.tsx
@@ -0,0 +1,100 @@
+import {
+	MessagesSquare,
+	BottleWine,
+	Tv,
+	Youtube,
+	Apple,
+	Goal,
+	Feather,
+	BookImage,
+} from 'lucide-react'
+
+const Footer = () => {
+	return (
+		<>
+			<footer className='h-[430px] border border-t-gray-600/40 mx-15 mt-20'>
+				<section className='flex my-9 gap-x-20'>
+					<div className='flex flex-col w-[400px]'>
+						<p className='text-gray-100 font-semibold text-xl'>Техническая поддержка</p>
+						<span className='text-gray-200/50 text-xl mt-3 font-semibold line-clamp-2 leading-5'>
+							Мы с удовольствием ответим на интересующие тебя вопросы
+						</span>
+						<span className='text-gray-200/50 text-xl mt-3 font-semibold underline'>
+							suppory@wink.ru
+						</span>
+						<button className='py-3 px-5 rounded-3xl w-[190px] text-white mt-4 font-bold flex bg-gradient-to-r from-[#c57020] to-[#ca491e]'>
+							<MessagesSquare className='mr-2' />
+							Написать в чат
+						</button>
+						<div className='flex gap-x-4 mt-10'>
+							<button className='bg-gray-900/80 rounded-lg p-1'>
+								<BottleWine fill='white' size={35} />
+							</button>
+							<button className='bg-gray-900/80 rounded-lg p-1'>
+								<Tv fill='white' size={35} />
+							</button>
+							<button className='bg-gray-900/80 rounded-lg p-1'>
+								<Youtube fill='white' size={35} />
+							</button>
+						</div>
+					</div>
+					<div className='flex flex-col w-[400px]'>
+						<p className='text-gray-100 font-semibold text-xl'>Помощь</p>
+						<span className='text-gray-200/50 text-xl mt-3 font-semibold'>Информация</span>
+						<span className='text-gray-200/50 text-xl mt-3 font-semibold'>Вопросы и ответы</span>
+						<span className='text-gray-200/50 text-xl mt-3 font-semibold'>
+							Установка приложения
+						</span>
+						<span className='text-gray-200/50 text-xl mt-4 font-semibold'>Общие вопросы</span>
+					</div>
+					<div className='flex flex-col w-[400px]'>
+						<p className='text-gray-100 font-semibold text-xl'>Wink</p>
+						<span className='text-gray-200/50 text-xl mt-3 font-semibold'>Контакты</span>
+						<span className='text-gray-200/50 text-xl mt-3 font-semibold'>
+							Активировать промокод
+						</span>
+						<span className='text-gray-200/50 text-xl mt-3 font-semibold'>Документы</span>
+						<span className='text-gray-200/50 text-xl mt-3 font-semibold'>
+							Пользовательское соглашение
+						</span>
+						<span className='text-gray-200/50 text-xl mt-3 font-semibold leading-5'>
+							Правила применения рекомендательных технологий
+						</span>
+						<span className='text-gray-200/50 text-xl mt-3 font-semibold'>
+							Подарки от наших партнеров
+						</span>
+					</div>
+					<div className='flex flex-col w-[160px] text-gray-100 font-semibold text-lg gap-y-3'>
+						<button className='w-[150px] h-[48px] bg-gray flex bg-gray-200/25 rounded-3xl p-1.5 pl-3 items-center'>
+							<Apple className='mr-1.5' fill='white' />
+							App Store
+						</button>
+						<button className='w-[150px] h-[48px] bg-gray flex bg-gray-200/25 rounded-3xl p-1.5 pl-3 items-center'>
+							<Goal className='mr-1.5' />
+							Google Play
+						</button>
+						<button className='w-[150px] h-[48px] bg-gray flex bg-gray-200/25 rounded-3xl p-1.5 pl-3 items-center'>
+							<Feather className='mr-1.5' />
+							RuStore
+						</button>
+						<button className='w-[150px] h-[48px] bg-gray flex bg-gray-200/25 rounded-3xl p-1.5 pl-3 items-center'>
+							<BookImage className='mr-1.5' />
+							AppGallery
+						</button>
+						<button className='w-[150px] h-[48px] bg-gray flex bg-gray-200/25 rounded-3xl p-1.5 pl-3 items-center'>
+							<Tv className='mr-1.5' />
+							Smart TV
+						</button>
+					</div>
+				</section>
+				<div className='border border-t-gray-600/40 h-[65px] flex items-center'>
+					<span className='text-gray-400/50 font-semibold text-lg'>
+						&copy; 2018 - 2025 Видеосервис Wink. Все права защищены.
+					</span>
+				</div>
+			</footer>
+		</>
+	)
+}
+
+export default Footer
--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,15 +1,65 @@
-import { useEffect } from 'react'
 import Header from '@components/UI/Header'
+import Footer from '@components/UI/Footer'
 
 const HomePage = () => {
-	useEffect(() => {
-		console.log('HomePage render')
-	}, [])
-
 	return (
 		<>
 			<Header />
-			<div className='w-full h-full bg-black text-red-500'></div>
+			<main className='bg-black'>
+				<article className='w-full h-[900px] relative'>
+					<video
+						className='absolute w-full h-full object-cover'
+						src='/bg_video.mp4'
+						autoPlay
+						muted
+						loop
+					/>
+					<div className='absolute right-3/10 top-3/10 bg-gray-500/65 w-[420px] p-4 z-10 rounded-3xl'>
+						<div className='flex flex-col'>
+							<div className='flex flex-col text-5xl text-white font-bold mb-6 leading-9'>
+								<p>Финал</p>
+								<p>&laquo;Фишер.</p>
+								<p>Затмение&raquo;</p>
+							</div>
+							<span className='text-gray-200 font-semibold mb-1'>
+								Шокирующая развязка детектива с Иваном
+							</span>
+							<span className='text-gray-200 font-semibold mb-8'>
+								Янковским в подписке &laquo;Wink Всё в одном&raquo;
+							</span>
+							<button className='w-full py-3 px-8 rounded-3xl bg-gradient-to-r from-[#ff922c] to-[#ff6431] text-white text-lg font-semibold'>
+								Подключить и смотреть
+							</button>
+							<span className='mt-3 text-[12px] text-gray-200'>
+								Отключить можно в любой момент в Личном кабинете
+							</span>
+						</div>
+					</div>
+				</article>
+				<section className='flex flex-col'>
+					<div className='ml-15'>
+						<p className='font-bold text-4xl text-white my-9 tracking-tighter'>
+							Александр Петров против монстра
+						</p>
+						<span className='text-gray-400 text-lg'>
+							Главный блокбастер года: эпическое противостояние подводников и хитроумного чудовища
+							из морских глубин
+						</span>
+						<div className='flex gap-4 mt-8'>
+							{[...Array(5)].map((_, index) => (
+								<div key={index} className='w-90 h-90 bg-gray-800 rounded-4xl overflow-hidden'>
+									<img
+										src={`/public/images_cut_1/image_part_00${index + 1}.jpg`}
+										alt={`Image ${index + 1}`}
+										className='w-full h-full object-cover'
+									/>
+								</div>
+							))}
+						</div>
+					</div>
+				</section>
+				<Footer />
+			</main>
 		</>
 	)
 }
